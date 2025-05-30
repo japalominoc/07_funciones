6ab91db6a7e6f50bfa249e08516157aefc029bd7
japalominoc <japalominoc@unjbg.edu.pe>
2025-05-29 19:09:59 -0500

    Actualizando ejercicio 2: Optimizacion del calculo del MCD usando algoritmo de Euclides y uso de restricciones para las variables de entrada (ser diferentes que 0)

--- 2_ejercicio.cpp
+++ 2_ejercicio.cpp
@@ -9,21 +9,26 @@ int main(){
 	cin>>num1;
 	cout<<"Ingrese el segundo numero: ";
 	cin>>num2;
-	
-	MCD=Calcular_MCD(num1,num2);
-	mcm=(num1*num2)/MCD;
+	if(num1!=0 && num2!=0){
+		MCD=Calcular_MCD(num1,num2);
+		mcm=(num1*num2)/MCD;
+	}
+	else{
+		cout<<"ERROR: Introduzca numeros diferentes que 0";
+		return 0;
+	}
 	cout<<"------------------------------"<<endl;
 	cout<<"El mcm es: "<<mcm<<endl;
 	cout<<"El MCD es: "<<MCD<<endl;
 	return 0;
 } 
 int Calcular_MCD(int a,int b){
-	int MCD, i=1;
-	while(i<=a && i<=b){
-		if(a % i==0 && b % i==0){
-			MCD=i;
-		}	
-		i=i+1;
-	}
-	return MCD;
+	int c;
+	 while (b != 0) {
+        a = a % b;
+        c = a;
+        a = b;
+        b = c;
+    }
+    return a;
 }
