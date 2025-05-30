e1e0b02b6ffeb1b7114da25bfc5cc1ed3188b484
japalominoc <japalominoc@unjbg.edu.pe>
2025-05-30 16:10:21 -0500

    Actualizando ejercicio 3: Algunas mejoras visuales y validaciones para los variables de entrada(no ser negativos)

--- 3_ejercicio.cpp
+++ 3_ejercicio.cpp
@@ -4,11 +4,21 @@ using namespace std;
 int factorial(int);
 int main(){
 	int m,n,comb;
-	cout<<"Ingrese el total de elementos: ";
+	cout<<"-----------------COMBINACIONES---------------------"<<endl;
+	cout<<"Ingrese el total de elementos(m>=0): ";
 	cin>>m;
-	cout<<"Ingrese la cantidad de elementos a elegir: ";
+	cout<<"Ingrese la cantidad de elementos a elegir(n>=0): ";
 	cin>>n;
+	if(m<0 || n<0){
+		cout<<"ERROR: los valores de m y n deben ser mayores a 0"<<endl;
+		return 1;
+	}
+	else if(n>m){
+		cout<<"ERROR: n debe ser menor que m "<<endl;
+		return 1;
+	}
 	comb=factorial(m)/((factorial(n))*(factorial(m-n)));
+	cout<<"----------------------------------------"<<endl;
 	cout<<"El valor de la combinatoria es: "<<comb;
 	return 0;
 }
