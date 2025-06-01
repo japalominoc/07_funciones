89e2c22a63381a92d46133e9ae6cecc329a3e316
japalominoc <japalominoc@unjbg.edu.pe>
2025-05-31 20:08:57 -0500

    Actualizando ejercicio 4: Validaciones para el valor de entrada(ser mayores a 1)

--- 4_ejercicio.cpp
+++ 4_ejercicio.cpp
@@ -1,26 +1,29 @@
 #include<iostream>
 using namespace std;
-
 int cal_divisores(int);
 int main(){
-        int num,cd=0;
-        cout<<"Ingrese un numero: ";
-        cin>>num;
-        cd=cal_divisores(num);
-        if(cd==2){
-            cout<<num<<" es primo"<<endl;
-        }
-        else{
-            cout<<num<<" no es primo"<<endl;
-        }
-        return 0;
+	int num,cd=0;
+	cout<<"Ingrese un numero: ";
+	cin>>num;
+	if(num<=1){
+		cout<<num<<" no es primo"<<endl;
+		return 0;
+	}
+	cd=cal_divisores(num);
+	if(cd==2){
+		cout<<num<<" es primo"<<endl;
+	}
+	else{
+		cout<<num<<" no es primo"<<endl;		
+	}
+	return 0;
 }
 int cal_divisores(int a){
-        int cd=0;
-        for(int i=1;i<=a;i++){
-            if(a % i==0){
-               cd=cd+1;
-            }
-        }
-        return cd;
+	int cd=0;
+	for(int i=1;i<=a;i++){
+		if(a % i==0){
+			cd=cd+1;
+		}
+	}
+	return cd;
 }
